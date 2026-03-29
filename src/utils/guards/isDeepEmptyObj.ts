a09c60f2e0a48479b282440cce7f2b62b08281ce
Torathion <torathion@gmail.com>
2026-03-29 23:45:49 +0200

    feat: add utility isDeepEmptyObj

---
+++ src/utils/guards/isDeepEmptyObj.ts
@@ -0,0 +1,25 @@
+import type { Obj } from "typestar"
+import { isArray } from "src/constants"
+import isObj from "./isObj"
+
+/**
+ * Checks if an object is empty (has no enumerable own properties).
+ *
+ * @param obj - The object to check for emptiness.
+ * @returns `true` if the object is empty or not a valid object (e.g., null or undefined), `false` otherwise.
+ */
+export default function isDeepEmptyObj(obj: Obj): boolean {
+  if (!obj) return true
+  /* eslint-disable  guard-for-in */
+  for (const x in obj) {
+    const prop = obj[x]
+    if (isObj(prop)) {
+      if (!isDeepEmptyObj(prop)) return false
+    }
+    else if (isArray(prop)) {
+      if (!isDeepEmptyObj(prop)) return false
+    } else if (prop != null) return false
+  }
+  /* eslint-enable  guard-for-in */
+  return true
+}
