b5c8e6734a4d2b7c1eb90bb7062ee68814c9556f
Torathion <torathion@gmail.com>
2026-03-30 17:10:30 +0200

    feat: add occurrences

---
+++ src/utils/obj/occurrences.ts
@@ -0,0 +1,18 @@
+import type { Obj } from 'typestar'
+
+/**
+ *  Counts all occurrences of all property values of a given property name in an array of objects.
+ *
+ * @param arr - target array of objects.
+ * @param prop - the target property name
+ * @returns all occurrences of each value in a map.
+ */
+export default function occurrences<T extends Obj>(arr: T[], prop: keyof T): Map<T[keyof T], number> {
+  const map = new Map<T[keyof T], number>()
+  for (const item of arr) {
+    const val = item[prop]
+    map.set(val, (map.get(val) || 0) + 1)
+  }
+
+  return map
+}
