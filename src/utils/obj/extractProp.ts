0b72203b4cda1b2853b68c8c3c7e5975adabe893
Torathion <torathion@gmail.com>
2026-03-30 14:22:38 +0200

    feat: add extractProp

---
+++ src/utils/obj/extractProp.ts
@@ -0,0 +1,17 @@
+import { hasProp } from "src/constants"
+import type { Table } from "typestar"
+
+/**
+ *  Extracts a property from a given object and deletes said property from it.
+ *
+ * @param obj - target object to extract from
+ * @param key - property key of the property to extract.
+ * @returns
+ */
+export default function extractProp<T, U extends Table<T>>(obj: U, key: keyof U): T | undefined {
+  if (hasProp(obj, key)) {
+    const value = obj[key]
+    delete obj[key]
+    return value
+  }
+}
